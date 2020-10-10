9dc468fd344450210bea2c28ae79a39ddb42c8a0
Sergey Zolotarev <sryze@protonmail.com>
2020-10-10 22:50:04 +0600

    Include domain in menu item's title and auto-reload tab after deletion

--- background.js
+++ background.js
@@ -1,44 +1,98 @@
-function deleteCookies(domain) {
-    chrome.cookies.getAll({
-        domain: domain
-    }, cookies => {
+let activeTabId = null;
+
+function getDomain(url) {
+    if (url instanceof URL) {
+        return url.hostname;
+    } else {
+        return new URL(url.toString()).hostname;
+    }
+}
+
+function deleteCookies(cookies, callback) {
+    let deleteCount = 0;
+
+    function removeCallback() {
+        if (++deleteCount >= cookies.length) {
+            if (callback) {
+                callback(deleteCount);
+            }
+        }
+    }
+
+    if (!cookies || cookies.length == 0) {
+        callback(0);
+    } else {
         for (const cookie of cookies) {
             chrome.cookies.remove({
                 url: 'http://' + cookie.domain + cookie.path,
                 name: cookie.name,
                 storeId: cookie.storeId
-            });
-            chrome.cookies.remove({
-                url: 'https://' + cookie.domain + cookie.path,
-                name: cookie.name,
-                storeId: cookie.storeId
-            });
+            }, removeCallback);
         }
+    }
+}
+
+function deleteCookiesForDomain(domain, callback) {
+    chrome.cookies.getAll({
+        domain: domain
+    }, cookies => {
+        deleteCookies(cookies, callback);
     });
 }
 
-function onDeleteCookiesClicked(info, tab) {
+function updateMenusInternal(activeTab) {
+    if (!activeTab.url) {
+        // Can happen if user switched to a chrome:// tab or something like this.
+        return;
+    }
+    chrome.contextMenus.update('delete', {
+        title: 'Delete cookies for ' + getDomain(activeTab.url)
+    });
+}
+
+function updateMenus(tab) {
+    if (tab != null && tab.id == activeTabId) {
+        updateMenusInternal(tab);
+    } else if (activeTabId != null) {
+        chrome.tabs.get(activeTabId, tab => {
+            updateMenusInternal(tab);
+        });
+    }
+}
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
     if (!tab.active) {
         return;
     }
     if (info.menuItemId == 'delete') {
-        const url = tab.url;
-        const domain = new URL(url).hostname;
         try {
-            deleteCookies(domain);
-            alert('Deleted cookies for ' + domain + '!');
+            const domain = getDomain(tab.url);
+            deleteCookiesForDomain(domain, deletedCount => {
+                if (deletedCount == 0) {
+                    alert('No cookies found for ' + domain + '!');
+                } else {
+                    alert('Deleted ' + deletedCount + ' ' (deleteCount == 1 ? 'cookie' : 'cookies') + ' for ' + domain + '!');
+                    chrome.tabs.reload(tab.id);
+                }
+            });
         } catch (e) {
-            alert('Could not delete cookies for ' + domain + ': ' + e.message);
+            alert('Error deleting cookies for ' + domain + ': ' + e.message);
         }
     }
-}
+});
 
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.contextMenus.removeAll();
-    chrome.contextMenus.create({
-        id: 'delete',
-        title: 'Delete cookies for this site',
-        contexts: ['browser_action', 'page_action', 'action']
-    });
-    chrome.contextMenus.onClicked.addListener(onDeleteCookiesClicked);
+chrome.contextMenus.removeAll();
+chrome.contextMenus.create({
+    id: 'delete',
+    title: 'Delete cookies for this site',
+    contexts: ['browser_action', 'page_action', 'action']
+});
+
+chrome.tabs.onActivated.addListener(activeInfo => {
+    activeTabId = activeInfo.tabId;
+    updateMenus();
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    updateMenus(tab);
 });
